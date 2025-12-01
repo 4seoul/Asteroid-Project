e862bbaf54e4cce923eac1d4e3521a805f2e4572
4seoul <a6ictive@gmail.com>
2025-12-01 13:02:43 +0000

    CH3 L5

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         log_state()
@@ -14,10 +25,17 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+
         screen.fill("black")
+
+        for drawables in drawable:
+            drawables.draw(screen)
+
         pygame.display.flip()
 
+        # limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
-
