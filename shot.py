8b5f5d40ed1d2032b4bcc46e5025476dea70aacc
obg <a6ictive@gmail.com>
2026-02-06 12:45:34 +0000

    adding comments

--- shot.py
+++ shot.py
@@ -7,8 +7,8 @@ class Shot(CircleShape):
         super().__init__(x, y, radius)
 
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH) # drawing the bullets shot from the spaceship
 
     def update(self, dt):
-        self.position += self.velocity * dt
+        self.position += self.velocity * dt # updating it position 
 
