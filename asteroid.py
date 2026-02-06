e7368031e5b360061ac980c873ad20897c64a5f8
obg <a6ictive@gmail.com>
2026-02-06 12:31:25 +0000

    adding comments

--- asteroid.py
+++ asteroid.py
@@ -10,26 +10,26 @@ class Asteroid(CircleShape):
         super().__init__(x, y, radius)
 
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH) # Drawing the asteroid 
 
     def update(self, dt):
-        self.position += self.velocity * dt
+        self.position += self.velocity * dt # Moving the asteroid
 
     def split(self):
         self.kill()
         if self.radius <= ASTEROID_MIN_RADIUS:
-            return "this was a small asteroid and we're done."
+            return "this was a small asteroid and we're done." # If its size is too small it deletes itself
 
-        log_event("asteroid_split")
+        log_event("asteroid_split") # splits the asteroid into 2 smaller asteroids
         radius = self.radius
         rand = random.uniform(20, 50)
         mov1 = pygame.math.Vector2.rotate(self.velocity, rand)
         mov2 = pygame.math.Vector2.rotate(self.velocity, -rand)
         new_radius = radius - ASTEROID_MIN_RADIUS
 
-        new_asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+        new_asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius) # initialising the new smaller asteroids
         new_asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
 
-        new_asteroid_1.velocity = mov1 * 1.2
+        new_asteroid_1.velocity = mov1 * 1.2 # setting the velocity of the new asteroids
         new_asteroid_2.velocity = mov2 * 1.2
 
